778d10035244c7714bffcd2a2f57540e18779652
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-11-26 07:30:20 +0100

    [2021-11-26] Daily (automated) update DLM mortality analyses

--- data-misc/excess_mortality/parse_cbs_links.R
+++ data-misc/excess_mortality/parse_cbs_links.R
@@ -27,3 +27,45 @@ imgsrc <- read_html(u) %>%
   html_attr('src')
 
 download.file(paste0("https://www.rivm.nl/",imgsrc), destfile="data-misc/excess_mortality/plots_weekly_update/sterfte_perweek_rivm.jpeg", mode="wb")
+
+
+
+
+## Download RIVM excess mortality data ##
+
+
+rivm_excess_data <- read.csv("data-misc/excess_mortality/excess_mortality_rivm.csv")
+
+jaar <- as.numeric(data.table::year(Sys.Date()))
+week <- data.table::week(Sys.Date())-2
+
+
+start.week <- as.Date(Sys.Date()-15)
+eind.week <- as.Date(Sys.Date()-8)
+
+url <- "https://www.rivm.nl/monitoring-sterftecijfers-nederland"
+webpage <- read_html(url)
+
+webpage.text <- webpage %>%
+  html_nodes("p") %>%
+  html_text()
+
+webpage.text <- webpage.text[2]
+webpage.text <- gsub('[.]', '', webpage.text)
+
+webpage.text.test <- parse_number(webpage.text)
+
+
+matches <- regmatches(webpage.text, gregexpr("[[:digit:]]+", webpage.text))
+numbers <- as.numeric(unlist(matches))
+
+sterfte <- numbers[6]
+ondergrens <- numbers[7]
+bovengrens <- numbers[8]
+midpoint <- (ondergrens+bovengrens)/2
+oversterfte <- sterfte-midpoint
+
+df.test <- as.data.frame(cbind(jaar, week, start.week, eind.week, ondergrens,bovengrens,sterfte,oversterfte,midpoint))
+
+rivm.data.merge <- rbind(rivm_excess_data,df.test)
+write.csv(rivm.data.merge, file = "data-misc/excess_mortality/excess_mortality_rivm.csv", row.names = F)
