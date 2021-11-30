23132bef9e15872c2b047f65be88ba7cd675f8ab
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2021-11-30 13:57:29 +0100

    Add NICE capacity for under 20 - @mdradvies

---
+++ workflow/dashboards/nice_bezetting_onder20.R
@@ -0,0 +1,26 @@
+temp = list.files(path = "data-nice/age/Clinical_Beds/",pattern="*.csv", full.names = T) ## Fetch all day files
+myfiles = lapply(temp, read.csv) ## Load all day files
+
+nice_by_day_clinical <- map_dfr(myfiles, ~{ ## Write dataframe of all day files
+  .x
+})
+
+nice_by_day_clinical <- nice_by_day_clinical %>%
+  filter(Leeftijd == "<20")
+
+
+temp = list.files(path = "data-nice/age/IC/",pattern="*.csv", full.names = T) ## Fetch all day files
+myfiles = lapply(temp, read.csv) ## Load all day files
+
+
+nice_by_day_ic <- map_dfr(myfiles, ~{ ## Write dataframe of all day files
+  .x
+})
+
+nice_by_day_ic <- nice_by_day_ic %>%
+  filter(Leeftijd == "<20")
+
+
+write.csv(nice_by_day_clinical,"data-dashboards/bezetting_klinisch_onder20.csv", row.names = F)
+write.csv(nice_by_day_ic,"data-dashboards/bezetting_ic_onder20.csv", row.names = F)
+
