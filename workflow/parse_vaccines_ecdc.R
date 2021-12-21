1217b4ebc76bd7b38185faf73308d6849ba8b274
Zelst <marino.vanzelst@wur.nl>
2021-12-21 08:58:28 +0100

    Add vaccine tracker with ECDC data

---
+++ workflow/parse_vaccines_ecdc.R
@@ -0,0 +1,24 @@
+vaccines <- fread("https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv")
+
+vaccines.nl <- vaccines %>%
+  dplyr::filter(Region == "NL") %>%
+  select(YearWeekISO,Vaccine,FirstDose, SecondDose,DoseAdditional1)
+  
+vaccines.nl.long <- vaccines.nl %>%
+  gather(key = "dose_number", value = "total_administered",
+         FirstDose, SecondDose,DoseAdditional1)
+
+
+vaccines.nl.long <- vaccines.nl.long %>%
+  mutate(vaccine = recode(Vaccine, "JANSS" = "Johnson&Johnson",
+                          "COM" = "Pfizer/BioNTech",
+                          "MOD" = "Moderna",
+                          "AZ" = "Oxford/AstraZeneca")) %>%
+  mutate(dose_number = parse_number(recode(dose_number, "FirstDose" = "1",
+                              "SecondDose" = "2",
+                              "DoseAdditional1" = "3"))) %>%  
+  mutate(week = parse_number(str_sub(YearWeekISO, 7, 8))) %>%
+  mutate(year = parse_number(str_sub(YearWeekISO, 1,4))) %>%
+  select(week, year, vaccine, dose_number, total_administered)
+
+write.csv(vaccines.nl.long, file = "data-rivm/vaccines-ecdc/vaccines_administered_nl.csv",row.names=F)
