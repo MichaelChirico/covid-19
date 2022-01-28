506dff6fb77803aaea7cd3c09e25c58f06b8406e
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2022-01-28 14:00:43 +0100

    Update NICE age-distribution in hospital 2022-01-28

--- workflow/parse_rivm_outage.R
+++ workflow/parse_rivm_outage.R
@@ -1,6 +1,5 @@
+dat.outage <- read.csv("data-misc/data_rivm_outages.csv")
 u <- "https://www.rivm.nl/coronavirus-covid-19/actueel"
-
-webshot(u, cliprect = c(10, 50, 950, 300), file = "plots/dagelijkse_storing.png")
 
 webpage <- read_html(u)
 
@@ -14,13 +13,14 @@ webpage.text <- gsub('[.]', '', webpage.text)
 webpage.text.test <- parse_number(webpage.text)
 matches <- regmatches(webpage.text, gregexpr("[[:digit:]]+", webpage.text))
 
-days.since <- matches[[1]][2]
+days.since <- last(dat.outage$days_since)+1
+
 infections.missing <- matches[[1]][2]
 dat.outage.today <- data.frame(as.Date(Sys.Date()),days.since,infections.missing)
 colnames(dat.outage.today) <- c("date","days_since","infections_missed")
 dat.outage.today$date <- as.Date(dat.outage.today$date)
 
-dat.outage <- read.csv("data-misc/data_rivm_outages.csv")
+
 dat.outage$date <- as.Date(dat.outage$date)
 dat.outage <- rbind(dat.outage,dat.outage.today)
 
