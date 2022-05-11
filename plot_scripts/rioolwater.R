24b7409f8d69af4a09d1bbb70bf92e6f0d85861e
Davy Landman <davy.landman@swat.engineering>
2022-05-11 23:22:09 +0200

    Switched to color-brewer scales

--- plot_scripts/rioolwater.R
+++ plot_scripts/rioolwater.R
@@ -7,6 +7,7 @@ library(ggthemes)
 library(ggalt)
 library(scales)
 require(viridis)
+library(geojsonio)
 
 fileName <- "misc/maps/vrgrenzen2020.geojson" # File for GGD map
 vrgrenzen <- geojson_read(fileName, what = "sp") # Load veiligheidsregio map
@@ -65,7 +66,36 @@ pop.df <- data.frame(id = idList, rioolwaarde = valueList, groei_percentage = po
 
 #colors <- c("-100% tot 0%" = "lightgreen", "0% tot 25%" = "yellow", "25% tot 50%" = "orange","50% tot 100%" = "magenta","100% tot 200%" = "red")
 
-colors <- c("-200% of lager" = "#86BADC","-100% tot 200%" = "#709DC8","-50% tot -100%" = "#517AC1", "-25% tot -50%" = "#4253A3", "0% tot -25%" = "#3F3FA7", "0% tot 25%" = "#4B3AAA", "25% tot 50%" = "#54298F","50% tot 100%" = "#742185","100% tot 200%" = "#891187", "200%+" = "#7B1251")
+#colors <- c("-200% of lager" = "#86BADC","-100% tot 200%" = "#709DC8","-50% tot -100%" = "#517AC1", "-25% tot -50%" = "#4253A3", "0% tot -25%" = "#3F3FA7", "0% tot 25%" = "#4B3AAA", "25% tot 50%" = "#54298F","50% tot 100%" = "#742185","100% tot 200%" = "#891187", "200%+" = "#7B1251")
+
+# RdBu colorbrewer
+# colors <- c(
+#   "-200% of lager" = "#053061",
+#   "-100% tot 200%" = "#2166ac",
+#   "-50% tot -100%" = "#4393c3", 
+#   "-25% tot -50%" = "#92c5de", 
+#   "0% tot -25%" = "#d1e5f0", 
+#   "0% tot 25%" = "#fddbc7", 
+#   "25% tot 50%" = "#f4a582",
+#   "50% tot 100%" = "#d6604d",
+#   "100% tot 200%" = "#b2182b", 
+#   "200%+" = "#67001f")
+
+# RdYlBu
+# colors <- c(
+#   "-200% of lager" = "#313695",
+#   "-100% tot 200%" = "#4575b4",
+#   "-50% tot -100%" = "#74add1", 
+#   "-25% tot -50%" = "#abd9e9", 
+#   "0% tot -25%" = "#e0f3f8", 
+#   "0% tot 25%" = "#fee090", 
+#   "25% tot 50%" = "#fdae61",
+#   "50% tot 100%" = "#f46d43",
+#   "100% tot 200%" = "#d73027", 
+#   "200%+" = "#a50026")
+
+
+
 
 #86BADC 23%, #709DC8 30%, #517AC1 36%, #4253A3 45%, #3F3FA7 51%, #4B3AAA 57%, #54298F 64%, #742185 70%, #891187 76%, #7B1251 83%, 
 
@@ -87,9 +117,12 @@ sewer.vr.map <- ggplot(data = vrDF) +
        subtitle = "Cijferwaardes: Gemiddelde aantal virusdeeltjes per 100.000 inwoners \n Kleuren: Relatieve groei/daling ten opzichte van vorige week",
        color = "Legend",
        caption = paste("Bron data: RIVM  | Plot: @mzelst | Datum laatst beschikbare data: ",format(last(sewer.data.vr.test$date),"%d-%b-%y"))) +
-  scale_color_manual(name = "Group",values = colors, labels = NULL, guide = "none") +
-  scale_fill_manual(values = colors, breaks = c("-200% of lager","-100% tot 200%","-50% tot -100%", "-25% tot -50%","0% tot -25%","0% tot 25%", "25% tot 50%", 
-                                                "50% tot 100%", "100% tot 200%","200%+"))
+  scale_fill_brewer(type="div", 
+                    direction =-1, 
+                    aesthetics = c("colour", "fill"), 
+                    palette = "RdBu", 
+                    drop = FALSE, 
+                    breaks = c("-200% of lager","-100% tot 200%","-50% tot -100%", "-25% tot -50%","0% tot -25%","0% tot 25%", "25% tot 50%", "50% tot 100%", "100% tot 200%","200%+"))
 
 sewer.vr.map + geom_text(data=pop.df, aes(label=paste0(rioolwaarde), x=long, y=lat), size = 3.5, colour="black",fontface="bold") +
   theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
@@ -97,6 +130,31 @@ sewer.vr.map + geom_text(data=pop.df, aes(label=paste0(rioolwaarde), x=long, y=l
         plot.subtitle = element_text(hjust = 0.5, size = 14))
 
 ggsave("plots/rioolwater_veiligheidsregio.png", width = 16, height = 8)
+
+
+# automatic interpolated color brewer scales
+
+sewer.vr.map <- ggplot(data = vrDF) +
+  geom_polygon(aes(x=long, y=lat, group = group, fill = groei_percentage), color="#2b2b2b", size = 0.15) +
+  coord_equal()+
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.pos = "right",
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        legend.title = element_blank()) +
+  labs(title = "Rioolwater per veiligheidsregio",
+       subtitle = "Cijferwaardes: Gemiddelde aantal virusdeeltjes per 100.000 inwoners \n Kleuren: Relatieve groei/daling ten opzichte van vorige week",
+       color = "Legend",
+       caption = paste("Bron data: RIVM  | Plot: @mzelst | Datum laatst beschikbare data: ",format(last(sewer.data.vr.test$date),"%d-%b-%y"))) +
+  scale_fill_distiller(type="div", aesthetics = c("colour", "fill"), palette = "RdBu", limits=c(-200,200))
+
+sewer.vr.map + geom_text(data=pop.df, aes(label=paste0(rioolwaarde), x=long, y=lat), size = 3.5, colour="black",fontface="bold") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
+        plot.title.position = "plot",
+        plot.subtitle = element_text(hjust = 0.5, size = 14))
+
+ggsave("plots/rioolwater_veiligheidsregio2.png", width = 16, height = 8)
 
 
 ## Gemeente sewer
