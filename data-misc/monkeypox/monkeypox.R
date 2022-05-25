222a8175698dfc308742c1c4134c831938556690
Marino van Zelst <marino.vanzelst@wur.nl>
2022-05-25 11:40:21 +0200

    Automate monkeypox script with web crawler RIVM webpage

--- data-misc/monkeypox/monkeypox.R
+++ data-misc/monkeypox/monkeypox.R
@@ -1,3 +1,5 @@
+require(rvest)
+
 setwd("C:/Users/ZELST007/Desktop/covid-19")
 monkeypox.nl <- fread("data-misc/monkeypox/monkeypox.csv")
 
@@ -6,11 +8,35 @@ monkeypox.nl <- monkeypox.nl %>%
     date = as.Date(Datum, tryFormats = c('%m/%d/%Y')),
     .before = Datum
   ) %>%
-  mutate(
-    Datum = NULL
-  ) 
+  mutate(Cumulatief = cumsum(Besmettingen)) %>%
+  mutate(Datum = NULL)
 
-title.monkeypox <- paste0("Cumulatief gemelde besmettingen met monkeypox in Nederland: ", max(monkeypox.nl$Cumulatief))
+rivm.webpage <- "https://www.rivm.nl/"
+
+websites <- rivm.webpage %>%
+  read_html() %>%
+  html_nodes("a") %>%
+  html_attr("href") 
+
+websites.numbered <- grep("monkeypox",websites)
+
+u <- paste0("https://www.rivm.nl",websites[websites.numbered[2]])
+
+new.cumulative.cases <- read_html(u) %>%
+  html_nodes("h1") %>%
+  html_text() %>%
+  parse_number()
+
+new.daily.cases <- last(monkeypox.nl$Cumulatief-new.cumulative.cases)
+
+new.obs <- data.frame(date = as.Date(Sys.Date()), Besmettingen = new.daily.cases, Cumulatief = new.cumulative.cases)
+
+monkeypox.nl <- rbind(monkeypox.nl,new.obs)
+
+
+title.monkeypox <- paste0("Cumulatief gemelde besmettingen met monkeypox in Nederland: ", max(monkeypox.nl$Cumulatief,na.rm=T))
+
+Sys.setlocale("LC_TIME", "dutch")
 
 monkeypox.nl %>%
   dplyr::filter(date() >= "2022-05-17") %>%
@@ -26,6 +52,8 @@ monkeypox.nl %>%
         plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
         plot.subtitle = element_text(hjust = 0.5, size = 12),
         plot.title.position = "plot",
-        plot.caption = element_text(size = 6)) +
-  labs(subtitle = "De datum van testafname is onbekend \n Ik rapporteer het aantal zoals het door het RIVM wordt gemeld per dag")
+        plot.caption = element_text(size = 12)) +
+  labs(subtitle = "De datum van testafname is onbekend",
+       caption = paste0("Bron: RIVM | Plot: @mzelst | Datum: ",as.Date(Sys.Date())))
+
 
