27e68224a64239a0adbadbce7b7298f32a7d3cf9
Marino van Zelst <marino.vanzelst@wur.nl>
2022-05-25 14:40:58 +0200

    Update monkeypox script for globaldothealth use

--- data-misc/monkeypox/monkeypox.R
+++ data-misc/monkeypox/monkeypox.R
@@ -1,37 +1,21 @@
 require(rvest)
 
 setwd("C:/Users/ZELST007/Desktop/covid-19")
-monkeypox.nl <- fread("data-misc/monkeypox/monkeypox.csv")
+monkeypox.nl <- fread("data-misc/monkeypox/monkeypox_globaldothealth.csv")
+
 
 monkeypox.nl <- monkeypox.nl %>%
   mutate(
-    date = as.Date(Datum, tryFormats = c('%m/%d/%Y')),
-    .before = Datum
+    date = as.Date(Date_confirmation, tryFormats = c('%m/%d/%Y')),
+    .before = Date_confirmation
   ) %>%
-  mutate(Cumulatief = cumsum(Besmettingen)) %>%
-  mutate(Datum = NULL)
+  mutate(Besmettingen = 1)
 
-rivm.webpage <- "https://www.rivm.nl/"
-
-websites <- rivm.webpage %>%
-  read_html() %>%
-  html_nodes("a") %>%
-  html_attr("href") 
-
-websites.numbered <- grep("monkeypox",websites)
-
-u <- paste0("https://www.rivm.nl",websites[websites.numbered[2]])
-
-new.cumulative.cases <- read_html(u) %>%
-  html_nodes("h1") %>%
-  html_text() %>%
-  parse_number()
-
-new.daily.cases <- last(monkeypox.nl$Cumulatief-new.cumulative.cases)
-
-new.obs <- data.frame(date = as.Date(Sys.Date()), Besmettingen = new.daily.cases, Cumulatief = new.cumulative.cases)
-
-monkeypox.nl <- rbind(monkeypox.nl,new.obs)
+monkeypox.nl <- aggregate(Besmettingen ~ date, data = monkeypox.nl, FUN = sum)
+monkeypox.nl <- pad(monkeypox.nl)
+monkeypox.nl <- monkeypox.nl %>%
+  mutate(Besmettingen = ifelse(is.na(Besmettingen),0,Besmettingen)) %>%
+  mutate(Cumulatief = cumsum(Besmettingen))
 
 
 title.monkeypox <- paste0("Cumulatief gemelde besmettingen met monkeypox in Nederland: ", max(monkeypox.nl$Cumulatief,na.rm=T))
@@ -54,6 +38,9 @@ monkeypox.nl %>%
         plot.title.position = "plot",
         plot.caption = element_text(size = 12)) +
   labs(subtitle = "De datum van testafname is onbekend",
-       caption = paste0("Bron: RIVM | Plot: @mzelst | Datum: ",as.Date(Sys.Date())))
+       caption = paste0("Bron: RIVM | Plot: @mzelst | Datum: ",as.Date(Sys.Date()))) +
+  ylim(0, max(monkeypox.nl$Cumulatief)+5) +
+  xlim(min(monkeypox.nl$date)-2, Sys.Date() + 1)
 
+write.csv(monkeypox.nl, file = "data-misc/monkeypox/monkeypox.csv", row.names = F)
 
