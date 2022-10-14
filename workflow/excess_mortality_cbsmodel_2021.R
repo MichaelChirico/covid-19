785bb2741f14f64ae689c29c178eb9a6907addef
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2022-10-14 05:09:51 +0200

    [2022-10-14] Daily (automated) update DLM mortality analyses

--- workflow/excess_mortality_cbsmodel_2021.R
+++ workflow/excess_mortality_cbsmodel_2021.R
@@ -11,7 +11,7 @@
 ## Put in double date breaker for daily update
 repeat {
   Sys.sleep(6)
-  time.start <- ymd_hms(paste0("2022-10-07"," 05:00:00"))
+  time.start <- ymd_hms(paste0("2022-10-14"," 05:00:00"))
   time.now <- ymd_hms(Sys.time())
   
   if (time.start < time.now){
